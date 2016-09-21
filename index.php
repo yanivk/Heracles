51b2a56a35f06ec565d717d9e4ab4cf1d8053b71
Yaniv KOUBBI (CHAMBLY) <YAKO@domafdb.ad>
2016-09-21 14:56:58 +0200

    page acueil fait sauf le footer

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include ('view/header.html'); ?>
+
+    
+        <?php include('view/menu.html');?>
+        <?php include('view/carousel.html'); ?>
+        
+        <?php include ('view/body.html'); ?>
+
+        <?php include ('view/footer.html'); ?>
+
